48a9f50e0b5ab65dd0ed1cb906bff35f02e6447f
seominyang <yangminse9@gmail.com>
2025-07-16 17:34:42 +0900

    feat: 영화 구조체

--- index.c
+++ index.c
@@ -48,6 +48,13 @@ struct theaterAddress {
 	int addresss;	// 서울 1, 인천 2, 경기 3. 지역넘버를 매겨서 사용
 };
 
+struct movie {
+	int num;
+	char title[100];
+	int rating;
+	char genre[50];
+};
+
 int main(void) {
 
 }
